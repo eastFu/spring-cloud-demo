a550e324508bf94dd2cbe80ec9a13342dc6a2faf
eastFu <492510931@qq.com>
2018-01-01 17:41:52 +0800

    spring cloud  : service-mhi demo core

--- service-hi/src/main/java/pers/east/springcloud/ServiceHiApplication.java
+++ service-hi/src/main/java/pers/east/springcloud/ServiceHiApplication.java
@@ -1,28 +1,61 @@
 package pers.east.springcloud;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 @RestController
 public class ServiceHiApplication {
+
+    private static final Logger LOG = Logger.getLogger(ServiceHiApplication.class.getName());
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceHiApplication.class, args);
 	}
 
+//
+//    @Value("${server.port}")
+//    String port;
+//
+//    @RequestMapping("/hi")
+//    public String home(@RequestParam String name) {
+//        return "hi "+name+",i am from port:" +port;
+//    }
 
-    @Value("${server.port}")
-    String port;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
 
     @RequestMapping("/hi")
-    public String home(@RequestParam String name) {
-        return "hi "+name+",i am from port:" +port;
+    public String callHome(){
+        LOG.log(Level.INFO, "calling trace service-hi  ");
+        return restTemplate.getForObject("http://localhost:8989/hi", String.class);
+    }
+    @RequestMapping("/info1")
+    public String info(){
+        LOG.log(Level.INFO, "calling trace service-hi ");
+        return "i'm service-hi";
+
+    }
+
+    @Bean
+    public AlwaysSampler defaultSampler(){
+        return new AlwaysSampler();
     }
 }
